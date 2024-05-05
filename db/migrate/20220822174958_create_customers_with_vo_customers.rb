676228331e5cd65121df640c6e4cf50a0b5be391
silva4dev <web4dev.lucas@outlook.com>
2024-05-05 01:16:19 -0300

    feat: add migrations

---
+++ db/migrate/20220822174958_create_customers_with_vo_customers.rb
@@ -0,0 +1,14 @@
+class CreateCustomersWithVoCustomers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :customers_with_vo_customers do |t|
+      t.string :phone
+      t.string :email
+      t.string :name
+      t.string :city
+      t.string :street
+      t.date :birthday
+
+      t.timestamps
+    end
+  end
+end
